b85768c828723223580ade3a44493bd1b9d3cc02
Lucas <monteiro9961@gmail.com>
2023-07-16 13:45:25 -0300

    Componentização

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Formulario from './components/Formulario';
+import Lista from './components/Lista';
 
 function App() {
   return (
     <div className="App">
-        Hello Word
+        <Formulario />
+        <Lista />
     </div>
   );
 }
